110d7a0e757d51f7726f9a428d551d2f32ccc22d
Joao Pedro Romao <ojpzinho@gmail.com>
2021-01-08 21:53:07 -0300

    index criado

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DOCKER</title>
+</head>
+<body>
+  <center>
+    <h1>Ambiente funcionando!</h1>
+
+    <img src="./docker.gif" width='800px' height="600px"/>
+  </center>
+</body>
+</html>
